2ac740c3697f02f80e77825c0206a12fc03c7253
Basma ALkerm <eng.balkerm@gmail.com>
2014-01-01 12:06:35 +0200

    set the app root to accounts page

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Ostaz2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'accounts#index'
 
   # See how all your routes lay out with "rake routes"
 
