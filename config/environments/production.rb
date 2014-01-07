0d3f4c54188e12d931aa54a1625f728b9bcc3472
Basma ALkerm <eng.balkerm@gmail.com>
2014-01-07 22:38:00 +0200

    add missing 'end'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,3 +65,4 @@ Ostaz2::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'ostaz-app.herokuapp.com'}
+  end
