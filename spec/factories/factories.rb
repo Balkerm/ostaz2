92b6304ab08f2e14e510a5c0110dcc6fc592bd43
Basma ALkerm <eng.balkerm@gmail.com>
2014-01-12 03:38:38 +0200

    follow 'http://stackoverflow.com/questions/3761622/rake-dbmigrate-produces-rake-aborted-could-not-find-table-error' to fix factory girl issue

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,10 +1,12 @@
 FactoryGirl.define do
   factory :role_accountant, class: Role do
     name 'Accountant'
+	initialize_with { Role.find_or_create_by_name('Accountant')}
   end
   
   factory :role_owner , class: Role do
     name 'Owner'
+	initialize_with { Role.find_or_create_by_name('Owner')}
   end
 
 ################################################################################
@@ -40,38 +42,38 @@ FactoryGirl.define do
   end
   
   factory :accountant , class: User  ,parent: :user do
-	roles [FactoryGirl.create(:role_accountant)]
+	roles {[FactoryGirl.create(:role_accountant)]}
   end
   
   factory :owner ,class: User , parent: :user do
-	roles [FactoryGirl.create(:role_owner)]
+	roles {[FactoryGirl.create(:role_owner)]}
   end
 ############################################################################3
   factory :account_main_assets, class: Account do
 	name  'total_Assets'
 	#AccountType  AccountType.find_by_name('Assets')
-    AccountType  FactoryGirl.create(:account_type_assets)
+    AccountType  {FactoryGirl.create(:account_type_assets)}
     #user  FactoryGirl.create(:accountant) 
 	balance  0
   end
   factory :account_main_liabilities, class: Account do
     name  'total_Liabilities'	
 	#AccountType  AccountType.find_by_name('Liabilities')
-    AccountType  FactoryGirl.create(:account_type_liabilities)
+    AccountType  {FactoryGirl.create(:account_type_liabilities)}
     #user  FactoryGirl.create(:accountant) 
 	balance  0
   end
   factory :account_main_equity, class: Account do
     name  'total_Equity'	
     #AccountType  AccountType.find_by_name('Equity')
-	AccountType  FactoryGirl.create(:account_type_equity)
+	AccountType  {FactoryGirl.create(:account_type_equity)}
     #user  FactoryGirl.create(:accountant) 
 	balance  0
   end
   factory :account_main_expenses, class: Account do
     name  'total_Expenses'	
 	#AccountType  AccountType.find_by_name('Expenses')
-    AccountType  FactoryGirl.create(:account_type_expenses)
+    AccountType  {FactoryGirl.create(:account_type_expenses)}
     #user  FactoryGirl.create(:accountant) 
 	balance  0
   end
@@ -81,7 +83,7 @@ FactoryGirl.define do
 		name  'acc_expensses'
 	end
 	#AccountType  AccountType.find_by_name('Assets')
-	AccountType   FactoryGirl.create(:account_type_assets)
+	AccountType   {FactoryGirl.create(:account_type_assets)}
   end
   
   factory :account_liabilities, class: Account, parent: :account_main_liabilities do
@@ -90,7 +92,7 @@ FactoryGirl.define do
 		name  'acc_expensses'
 	end
 	#AccountType  AccountType.find_by_name('Liabilities')
-	AccountType   FactoryGirl.create(:account_type_liabilities)
+	AccountType   {FactoryGirl.create(:account_type_liabilities)}
   end
   factory :account_equity, class: Account,parent: :account_main_equity do
     ignore do
@@ -98,7 +100,7 @@ FactoryGirl.define do
 		name  'acc_expensses'
 	end	
 	#AccountType  AccountType.find_by_name('Equity')
-	AccountType  FactoryGirl.create(:account_type_equity)
+	AccountType  {FactoryGirl.create(:account_type_equity)}
 	
   end
   factory :account_expenses, class: Account, parent: :account_main_expenses do
@@ -107,7 +109,7 @@ FactoryGirl.define do
 		name  'acc_expensses'
 	end
 	#AccountType  AccountType.find_by_name('Expenses')
-	AccountType  FactoryGirl.create(:account_type_expenses)
+	AccountType  {FactoryGirl.create(:account_type_expenses)}
   end   
 end
 
